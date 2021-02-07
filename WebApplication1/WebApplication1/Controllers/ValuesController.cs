9173fcc028f2b839c6f07f98a07c7084bffd839e
ddllab <ddl.rybchenko@gmail.com>
2021-02-07 21:39:43 +0300

    *EmailSpammer

--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -5,6 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+public struct EmailSpammerReq
+{
+    public string to        {get; set;}
+    public string subject   {get; set;}
+    public string msg_text { get; set; } 
+}
+
+public struct EmailSpammerAck
+{
+    public EmailSpammerReq request { get; set; }
+    public string result { get; set; }
+}
 
 namespace WebApplication1.Controllers
 {
@@ -25,23 +37,18 @@ namespace WebApplication1.Controllers
         {
             return "value";
         }
+            
 
+        
         // POST api/<ValuesController>
+        [Route("send_spam")]
         [HttpPost]
-        public void Post([FromBody] string value)
+        public EmailSpammerAck Post([FromBody] EmailSpammerReq value)
         {
-        }
-
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            EmailSpammerAck emailSpammerAck = new EmailSpammerAck();
+            emailSpammerAck.request = value;
+            emailSpammerAck.result = "OK";
+            return emailSpammerAck;
         }
     }
 }
